59f74b99db39aa2e8b67df715d8de27d73c29ae6
Tim PC <isaiah.pwnpk@gmail.com>
2025-04-10 17:34:50 +0700

    Learning Vue.js with Net Ninja

---
+++ app.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            url: 'https://www.google.com',
+            showBooks: true,
+            books: [
+                {title: 'name of the wind', author: 'patrick rothfuss', img: "https://picsum.photos/200/300", isFav: true},
+                {title: 'the way of kings', author: 'brandon sanderson', img: "https://picsum.photos/200/300", isFav: false},
+                {title: 'the final empire', author: 'brandon sanderson', img: "https://picsum.photos/200/300", isFav: true}
+            ]
+        }
+    },
+    methods: {
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks;
+        }
+    }
+})
+
+app.mount('#app')
